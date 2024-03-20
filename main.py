73db14dca774a3baa3e4062fa392fe835edf9217
Natalia-Espitia <158238902+Natalia-Espitia@users.noreply.github.com>
2024-03-20 11:59:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ def probab_pos(v, p):
     c = (abs(v[p])) ** 2
     return c / norma
 
-def probab_transitar(v, k):
+print(probab_pos([-3 - 1j, -2j, 1j, 2], 2))
+
+def probabilidad_transitar(v, k):
     nv = np.linalg.norm(v)
     nk = np.linalg.norm(k)
-
     i = 0
     while i < len(v):
         v[i] = v[i] / nv
@@ -20,30 +21,8 @@ def probab_transitar(v, k):
     while i < len(v):
         prod_in += np.conj(k[i]) * v[i]
         i += 1
-
     prod_v = nv * nk
     prob = prod_in / prod_v
     return prob
-
-print(probab_pos([-3 - 1j, -2j, 1j, 2], 2))
-print(probab_transitar([1, 2, 3, 4, 2], [9, 1, 8, 7, 6]))
-
-def transicion(v1,v2):
-
-    norma_v1=np.linalg.norm(v1)
-    for i in range(len(v1)):
-        v1[i]=v1[i]/norma_v1
-        return v1
-    norma_v2= np.linalg.norm(v2)
-    for i in range(len(v2)):
-        v2[i] = v2[i] / norma_v2
-        return v2
-
-
-    for i in range(len(v1)):
-        v1[i] = v1[i].conjugate()
-        interno=np.inner(v1,v2)
-    return interno
-
-print(transicion([1,2,3],[4,5,6]))
+print(probabilidad_transitar([np.sqrt(2) / 2, np.sqrt(2) / 2, 0], [0, -np.sqrt(2) / 2, np.sqrt(2) / 2]))
 
